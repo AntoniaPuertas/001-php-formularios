709e2dd20b217a97b1e7f4d4de60be3cd496419a
Totilla <tsm.antonia@gmail.com>
2025-05-26 12:30:04 +0200

    datos sanitizados

--- validar.php
+++ validar.php
@@ -53,9 +53,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     }else{
         //se han recibido los datos y hay errores
         //Mostrar los errores por pantalla
-        mostrarErrores();
+        mostrarErrores($errores);
         //Mostrar un enlace para volver al formulario
-        
+        echo "<p><a href='javascript:history.back()'>Volver al formulario</a></p>";
     }
 
 }else{
@@ -66,9 +66,33 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 }
 
 function mostrarDatos(){
+    $comentarios = "";
+    if(empty($_POST['inputComentarios'])){
+        $comentarios = "Sin comentarios";
+    }else{
+        $comentarios = $_POST['inputComentarios'];
+    }
+    //$comentarios = empty($_POST['inputComentarios']) ? "Sin comentarios" : $_POST['inputComentarios'];
+    //TODO comprobar si la fecha es válida
+    $fecha = new DateTime($_POST['inputAntiguedad']);
+    echo "<h2>Datos recibidos:</h2>";
+    echo "<p><strong>Nombre: </strong>".htmlentities(htmlspecialchars($_POST['inputNombre'])) ."</p>";
+    echo "<p><strong>Dirección: </strong>". htmlspecialchars($_POST['inputDireccion']) ."</p>";
+    echo "<p><strong>Email: </strong>". htmlspecialchars($_POST['inputEmail']) ."</p>";
+    echo "<p><strong>Contraseña: </strong> Oculta por seguridad</p>";
+    echo "<p><strong>Antigüedad: </strong>". $fecha->format('d/m/Y') ."</p>";
+    //echo "<p><strong>Antigüedad: </strong>". $_POST['inputAntiguedad'] ."</p>";
+    echo "<p><strong>Comentarios: </strong>". htmlspecialchars($comentarios)  ."</p>"; 
+    echo "<p><strong>Aceptó políticas: </strong> Sí </p>";
+    
 
 }
 
-function mostrarErrores(){
-
+function mostrarErrores($errores){
+    echo "<h2>Errores en el formulario:</h2>";
+    echo "<ul>";
+    foreach($errores as $error){
+        echo "<li>$error</li>";
+    }
+    echo "</ul>";
 }
